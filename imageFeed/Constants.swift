066eb70be3ac9af4c48697ca3a6ee669fed0d73b
Kirill D <krldrn@icloud.com>
2024-08-04 09:14:30 +0200

    Настройка webView

---
+++ imageFeed/Constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+enum Constants {
+    
+    static let accessKey = "xf9zucHvfEyhtEsZxrWHeaI9Z3-r2sjuJsQze_4Wwj0"
+    
+    static let secretKey = "GfzoVQWgEVLfmmCNZM-54gNGG4YiQGuAcHzcR2r2NEc"
+    
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    
+    static let accessScope = "public+read_user+write_likes"
+    
+    static let defaultBaseURL = URL (string: "https://api.unsplash.com/")
+}
